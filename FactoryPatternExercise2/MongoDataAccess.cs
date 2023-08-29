ac9b5fcf534e225aa18c20c97053827e1009acd7
Bryson Varner <brysonandmellisa@hotmail.com>
2023-08-29 16:08:41 -0500

    Completed Factory-Pattern-Exercise-2

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class MongoDataAccess : Product, IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product(){Name = "VR Goggles", Price = 599.99},
+            new Product(){Name = "Xbox", Price = 499.99},
+            new Product(){Name = "PC Monitor", Price = 199.99},
+            new Product(){Name = "Chromebook", Price = 299.99}
+        };
+
+        List<Product> IDataAccess.LoadData()
+        {
+            Console.WriteLine($"I am reading data from the Mongo Database.");
+
+            return Products;
+        }
+
+        void IDataAccess.SaveData()
+        {
+            Console.WriteLine($"I am saving data to the Mongo Database.");
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string reply;
+            bool inputCheck = false;
+            do
+            {
+                Console.WriteLine("Hello, Which database would you want to work with?");
+                Console.WriteLine("Please enter either 'sql' 'mongo' or 'list'.");
+                reply = Console.ReadLine();
+                
+                if (reply.ToLower() != "sql" && reply.ToLower() != "list" && reply.ToLower() != "mongo")
+                {
+                    Console.WriteLine("Please enter a valid selection.");
+                }
+                else
+                {
+                    inputCheck = true;
+                }
+                
+            } while (!inputCheck);
+            var db = DataAccessFactory.GetDataAccessType(reply);
+
+            var products = db.LoadData();
+            db.SaveData();
+
+            foreach ( var product in products )
+            {
+                Console.WriteLine($"Item Name: {product.Name} Item Price: {product.Price}");
+            }
+
         }
     }
 }
